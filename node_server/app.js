21d48389b839136ff086c5fab0b876ed0c1ec9f7
Ranaco <ranasatyamraj@gmail.com>
2022-02-13 15:08:15 +0530

    Shifted to http

--- node_server/app.js
+++ node_server/app.js
@@ -2,12 +2,77 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
-const server = http.createServer(app);
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const key = require('./constants')
+
+const server = http.createServer((req, res) => {
+        console.log(req.url, req.method);
+
+        const extension = path.extname(req.url);
+
+        const serveFile = async (fileName, contentType, response) => {
+           try{
+            const file = await fsPromises.readFile(fileName, !contentType.includes('images') ? "utf8" : "");
+            response.writeHead(200, {'Content-Type': contentType});
+            response.end(file);
+           }catch(err){
+               console.log(err);
+           }
+        }
+
+        let contentType;
+
+        switch(extension){
+            case ".css":
+                contentType = "text/css";
+                break;
+            case ".html":
+                contentType = "text/html";
+                break;
+            case ".jpg":
+                contentType = "image/jpg";
+                break;
+            case ".png":
+                contentType = "image/png";
+                break;
+            case ".json":
+                contentType = "application/json";
+                break;
+            case ".txt":
+                contentType = "text/plain";
+                break;
+            default:
+                contentType = "text/html";
+        }
+
+        let filePath = contentType === "text/html" && req.url === "/" ?
+                            path.join(__dirname,"views", 'index.html'): 
+                                contentType === "text/html" && req.url.slice(-1) === "/" ?
+                                    path.join(__dirname,"views", req.url, 'index.html'):
+                                        contentType === "text/html" ?
+                                            path.join(__dirname, 'views', req.url):
+                                                path.join(__dirname, req.url);
+
+                        console.log(contentType, filePath);
+
+        if(!extension && req.url.slice(-1) !== "/") filePath +=".html";
+        const fileExists = fs.existsSync(__dirname, filePath);
+        
+        if(fileExists){
+                serveFile(path.join(filePath), contentType, res);
+        } else {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end('<h1>404 Not Found</h1>');
+        }
+
+})
+
 const io = require('socket.io')(server,{cors: {origin: '*'}});
 const mongo = require('mongoose');
-const mongoDbKey = "mongodb+srv://AsTaR_aNjE:messengerclone@cluster0.4oztt.mongodb.net/messenger_clone?retryWrites=true&w=majority";
 const Msg = require("./models/messages.js");
-mongo.connect(mongoDbKey, {useNewUrlParser: true, useUnifiedTopology: true,}).then(() => {
+mongo.connect(key, {useNewUrlParser: true, useUnifiedTopology: true,}).then(() => {
     console.log('mongodb connected');
 }).catch((err) => {
     console.log(err);
@@ -61,5 +126,9 @@ io.on('connection', (client) => {
 const PORT = process.env.PORT || 3500;
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port`, PORT);
+    console.log("listening on port", PORT);
 })
+
+// server.listen(PORT, () => {
+//     console.log(`Server is running on port`, PORT);
+// })
--- node_server/constants.js
+++ node_server/constants.js
@@ -1,1 +1,3 @@
-const mongoDbKey = ""
+const mongoDbKey = "mongodb+srv://AsTaR_aNjE:messengerclone@cluster0.4oztt.mongodb.net/messenger_clone?retryWrites=true&w=majority"
+
+module.exports = mongoDbKey;
